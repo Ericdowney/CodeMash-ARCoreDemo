945f115450b065f8b9dfd25d26e8d821ca42b900
Eric Downey <ericdowney@me.com>
2019-01-09 09:07:08 -0500

    Initial Commit

---
+++ app/src/main/java/com/downey/ardemo/MainActivity.java
@@ -0,0 +1,208 @@
+package com.downey.ardemo;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Plane;
+import com.google.ar.core.PointCloud;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+
+import com.downey.ardemo.Utilities.CameraPermissionHelper;
+import com.downey.ardemo.Rendering.*;
+
+import java.util.ArrayList;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
+
+    private GLSurfaceView surfaceView;
+
+    private boolean userRequestedInstall = true;
+
+    private Session session;
+
+    private final BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
+    private final PlaneRenderer planeRenderer = new PlaneRenderer();
+    private final PointCloudRenderer pointCloudRenderer = new PointCloudRenderer();
+
+    private final float[] anchorMatrix = new float[16];
+    private final ArrayList<ColoredAnchor> anchors = new ArrayList<>();
+
+    // Anchors created from taps used for object placing with a given color.
+    private static class ColoredAnchor {
+        public final Anchor anchor;
+        public final float[] color;
+
+        public ColoredAnchor(Anchor a, float[] color4f) {
+            this.anchor = a;
+            this.color = color4f;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        surfaceView = findViewById(R.id.surfaceview);
+
+        // Setup Renderer
+        surfaceView.setPreserveEGLContextOnPause(true);
+        surfaceView.setEGLContextClientVersion(2);
+        surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        surfaceView.setRenderer(this);
+        surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+        surfaceView.setWillNotDraw(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+
+        try {
+            session = configureARSession();
+            session.resume();
+            surfaceView.onResume();
+        }
+        catch (UnavailableUserDeclinedInstallationException e) {
+            Toast.makeText(this, "User Declined Installation: " + e, Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Generic Exception: " + e, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (session != null) {
+            surfaceView.onPause();
+            session.pause();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG).show();
+
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        try {
+            backgroundRenderer.createOnGlThread(this);
+            planeRenderer.createOnGlThread(this, "models/trigrid.png");
+            pointCloudRenderer.createOnGlThread(this);
+        }
+        catch (Exception e) {}
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        if (session == null) {
+            return;
+        }
+
+        try {
+            session.setCameraTextureName(backgroundRenderer.getTextureId());
+
+            Frame frame = session.update();
+            Camera camera = frame.getCamera();
+
+            backgroundRenderer.draw(frame);
+
+            if (camera.getTrackingState() == TrackingState.PAUSED) {
+                return;
+            }
+
+            // Get projection matrix.
+            float[] projmtx = new float[16];
+            camera.getProjectionMatrix(projmtx, 0, 0.1f, 100.0f);
+
+            // Get camera matrix and draw.
+            float[] viewmtx = new float[16];
+            camera.getViewMatrix(viewmtx, 0);
+
+            // Compute lighting from average intensity of the image.
+            // The first three components are color scaling factors.
+            // The last one is the average pixel intensity in gamma space.
+            final float[] colorCorrectionRgba = new float[4];
+            frame.getLightEstimate().getColorCorrection(colorCorrectionRgba, 0);
+
+            // Visualize tracked points.
+            PointCloud pointCloud = frame.acquirePointCloud();
+            pointCloudRenderer.update(pointCloud);
+            pointCloudRenderer.draw(viewmtx, projmtx);
+
+            // Application is responsible for releasing the point cloud resources after
+            // using it.
+            pointCloud.release();
+
+            // Visualize planes.
+            planeRenderer.drawPlanes(session.getAllTrackables(Plane.class), camera.getDisplayOrientedPose(), projmtx);
+
+            // Visualize anchors created by touch.
+            float scaleFactor = 1.0f;
+            for (ColoredAnchor coloredAnchor : anchors) {
+                if (coloredAnchor.anchor.getTrackingState() != TrackingState.TRACKING) {
+                    continue;
+                }
+                // Get the current pose of an Anchor in world space. The Anchor pose is updated
+                // during calls to session.update() as ARCore refines its estimate of the world.
+                coloredAnchor.anchor.getPose().toMatrix(anchorMatrix, 0);
+            }
+        }
+        catch (Exception e) {}
+    }
+
+    @Nullable
+    private Session configureARSession() throws Exception {
+        if (session == null) {
+            switch (ArCoreApk.getInstance().requestInstall(this, userRequestedInstall)) {
+                case INSTALLED:
+                    return new Session(this);
+                case INSTALL_REQUESTED:
+                    userRequestedInstall = false;
+                    break;
+            }
+        }
+        return session;
+    }
+}
+
